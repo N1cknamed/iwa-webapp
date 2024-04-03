b7b86144bad5b0572afbc409fdc442ecee16e7dc
Thomasa <60262650+JustThomasa@users.noreply.github.com>
2024-04-03 21:02:44 +0200

    Implemented SubscriptionFormType

--- src/Controller/ContractBeheerController.php
+++ src/Controller/ContractBeheerController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Subscription;
+use App\Form\SubscriptionFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -14,5 +18,21 @@ class ContractBeheerController extends AbstractController
         return $this->render('contract_beheer/index.html.twig', [
             'controller_name' => 'ContractBeheerController',
         ]);
+    }
+
+    #[Route('/contractbeheer/addsubscription', name: 'app_add_subscription')]
+    public function addSubscription(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $subscription = new Subscription();
+        $form = $this->createForm(SubscriptionFormType::class, $subscription);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager->persist($subscription);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_contract_beheer');
+        }
     }
 }
