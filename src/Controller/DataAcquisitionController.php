ffd0325e15bd35e61aae5f9d0dd4cf4039e86439
Robbie Kooistra <robbiekooistra2000@gmail.com>
2024-04-03 14:14:35 +0200

    Implement pagination for results

--- src/Controller/DataAcquisitionController.php
+++ src/Controller/DataAcquisitionController.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Station;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Pagination\Paginator;
+use Symfony\Component\HttpFoundation\Request;
 
 class DataAcquisitionController extends AbstractController
 {
@@ -18,15 +20,26 @@ class DataAcquisitionController extends AbstractController
     }
 
     #[Route('/dataacquisition', name: 'app_data_acquisition')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $stations = $this->entityManager
+        $currentPage = $request->query->getInt('page', 1);
+        $limit = 25;
+
+        $query = $this->entityManager
             ->getRepository(Station::class)
-            ->findBy([], null, 25); // Limit to 25 stations
+            ->createQueryBuilder('s')
+            ->getQuery();
+
+        $paginator = new Paginator($query);
+        $paginator->getQuery()
+            ->setFirstResult($limit * ($currentPage - 1)) // Offset
+            ->setMaxResults($limit); // Limit
 
         return $this->render('data_acquisition/index.html.twig', [
-            'stations' => $stations,
+            'stations' => $paginator,
             'controller_name' => 'DataAcquisitionController',
+            'current_page' => $currentPage,
+            'total_pages' => ceil(count($paginator) / $limit),
         ]);
     }
 }
