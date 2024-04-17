7c8cf9f8fc10fd9ee3239425ab806e7e71ed52b8
jeroen <j.b.j.de.boer@st.hanze.nl>
2024-04-17 16:08:37 +0200

    homepagina aanpassingen

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,15 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        if ($this->isGranted('ROLE_DATA')) {
+            return $this->render('home/station_storingen.html.twig', [
+                'controller_name' => 'HomeController',
+            ]);
+        }
+        else {
+            return $this->render('home/index.html.twig', [
+                'controller_name' => 'HomeController',
+            ]);
+        }
     }
 }
