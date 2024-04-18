271ed06c9d5f5dd0b3831a9d52f3ac52d8dc45fa
Robbie Kooistra <robbiekooistra2000@gmail.com>
2024-04-19 01:32:39 +0200

    Update dashboard

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Contract;
 use App\Entity\Malfunction;
+use App\Entity\Subscription;
+use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,15 +20,37 @@ class HomeController extends AbstractController
         $this->entityManager = $entityManager;
     }
 
+    public function getSubscriptionCount(): int
+    {
+        return $this->entityManager->getRepository(Subscription::class)->count([]);
+    }
+
+    public function getContractCount(): int
+    {
+        return $this->entityManager->getRepository(Contract::class)->count([]);
+    }
+    public function getUserCount(): int
+    {
+        return $this->entityManager->getRepository(User::class)->count([]);
+    }
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         $malfunctions = $this->entityManager
             ->getRepository(Malfunction::class)
-            ->findBy(['status' => ['unresolved', 'in progress']]);
+            ->findBy(['status' => ['unresolved', 'in progress', 'resolved']]);
+
+        $subscriptionCount = $this->getSubscriptionCount();
+        $contractCount = $this->getContractCount();
+        $userCount = $this->getUserCount();
+
 
         return $this->render('home/home.html.twig', [
             'malfunctions' => $malfunctions,
+            'subscriptionCount' => $subscriptionCount,
+            'contractCount' => $contractCount,
+            'userCount' => $userCount,
             'controller_name' => 'HomeController',
         ]);
     }
