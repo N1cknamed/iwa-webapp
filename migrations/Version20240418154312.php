c3dbe2a4285f71857f0543c24d2defef90a8ae61
Robbie Kooistra <robbiekooistra2000@gmail.com>
2024-04-18 18:04:16 +0200

    Update language and styling for contractbeheer

---
+++ migrations/Version20240418154312.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240418154312 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE malfunction ADD CONSTRAINT FK_DB39BE79B010593B FOREIGN KEY (station_name) REFERENCES station (name)');
+        $this->addSql('CREATE INDEX IDX_DB39BE79B010593B ON malfunction (station_name)');
+        $this->addSql('ALTER TABLE message ADD CONSTRAINT FK_B6BD307FB010593B FOREIGN KEY (station_name) REFERENCES station (name)');
+        $this->addSql('ALTER TABLE weather ADD CONSTRAINT FK_4CD0D36EDE08BD3C FOREIGN KEY (STN) REFERENCES station (name)');
+        $this->addSql('CREATE INDEX IDX_4CD0D36EDE08BD3C ON weather (STN)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE messenger_messages');
+        $this->addSql('ALTER TABLE malfunction DROP FOREIGN KEY FK_DB39BE79B010593B');
+        $this->addSql('DROP INDEX IDX_DB39BE79B010593B ON malfunction');
+        $this->addSql('ALTER TABLE message DROP FOREIGN KEY FK_B6BD307FB010593B');
+        $this->addSql('ALTER TABLE weather DROP FOREIGN KEY FK_4CD0D36EDE08BD3C');
+        $this->addSql('DROP INDEX IDX_4CD0D36EDE08BD3C ON weather');
+    }
+}
